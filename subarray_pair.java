61db2b9c814890a0e56f3ee8dd4d272049c77706
Tapobrotagithub <106526182+Tapobrotagithub@users.noreply.github.com>
2023-10-15 19:16:16 +0530

    Update subarray_pair.java

--- subarray_pair.java
+++ subarray_pair.java
@@ -1,15 +1,18 @@
 public class subarray_pair {
     public static void subarray(int array[]){
+        int total = 0 ;
         for(int i=0 ;i<array.length ;i++){
-            for(int j=i+1 ;j<array.length ;j++){
+            for(int j=i ;j<array.length ;j++){
                 System.out.print("("+array[i]);
                 for(int k=i+1 ;k<=j ;k++){
                     System.out.print(","+array[k]) ;
                 }
                 System.out.print(")");
+                total++ ;
             }
             System.out.println();
         }
+        System.out.println("Total subarrays are : "+total);
 
 
     }
